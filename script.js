84440cd36643dd0f8b0b2619a75e61e5e09539dd
Michael_Interface <interfacemichael@gmail.com>
2025-12-04 21:19:27 +0300

    feat: confetti every time

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-// Check if user has visited before
-const hasVisited = localStorage.getItem("hasVisited");
+confetti({
+  particleCount: 150,
+  spread: 70,
+  origin: { y: 0.6 }
+});
 
-// If first time -> show confetti
-if (!hasVisited) {
-    confetti({
-      particleCount: 150,
-      spread: 70,
-      origin: { y: 0.6 }
-    });
-
-    // Mark that they visited
-    localStorage.setItem("hasVisited", "true");
-}
